3c9b66e1266f5066ef988f976c3f564d40fc8521
Aravind jinna <aravindjinna1@gmail.com>
2026-01-31 12:08:08 +0530

    final commit

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:[
+      "http://localhost:5173",
+       "https://task-management-rho-green.vercel.app/"
+    ]
+}
+
+));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));

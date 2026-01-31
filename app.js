2701113d992d4470c00be6055dbf39a2ab8ab6e3
Aravind jinna <aravindjinna1@gmail.com>
2026-01-31 14:37:42 +0530

    safe

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://task-management-rho-green.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://task-management-rho-green.vercel.app"
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true
+//   })
+// );
+
+// app.options("/*", cors());
+
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
+app.options("/*", cors());
+
 
 
 app.use(express.json());
